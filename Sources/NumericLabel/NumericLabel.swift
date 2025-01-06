949b772aeeea595bd9d805a5d236bc8880d66d6e
砍砍 <git@qaq.wiki>
2025-01-06 15:52:59 +0800

    update: copyright notice

--- Sources/NumericLabel/NumericLabel.swift
+++ Sources/NumericLabel/NumericLabel.swift
@@ -1,8 +1,6 @@
 //
-//  NumericLabel.swift
-//  NumericTransitionLabel
-//
-//  Created by 秋星桥 on 2025/1/6.
+//  Created by Lakr233 on 2025/1/6.
+//  Copyright (c) 2025 Lakr233. All rights reserved.
 //
 
 import NumericTransitionLabel
